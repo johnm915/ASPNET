30ec30b287a626ab7e8409769fa1248b4d5ae663
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-05-23 10:54:16 -0500

    finished View Product (2) and Update Product (3)

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using System.Data;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        
+        public Product GetProduct(int ID);
+        public void UpdateProduct(Product product);
+
+
+    }
+}
